0d7fea5a50d8bf1c0531853e7b315dd455b8e2b4
DerrickhSun <derrick.han.sun@gmail.com>
2020-02-14 20:52:08 -0800

    Update Invert.java

--- src/main/java/frc/robot/commands/Invert.java
+++ src/main/java/frc/robot/commands/Invert.java
@@ -10,27 +10,28 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.DriveTrain;
+//import frc.robot.subsystems.;
 
 public class Invert extends CommandBase {
   /**
    * Creates a new Invert.
    */
-  private DriveTrain drive;
+  //private DriveTrain drive;
   public Invert(DriveTrain drive) {
     // Use addRequirements() here to declare subsystem dependencies.
-    this.drive=drive;
+    //this.drive=drive;
     addRequirements(drive);
   }
 
 // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    drive.switchInverted();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    drive.switchInverted();
   }
 
   // Called once the command ends or is interrupted.
